94416ae96dd407ea29f17934beabcb047e7ecc34
Antoine07 <antoine.lucsko@gmail.com>
2024-07-04 15:14:44 +0200

    create trainer

--- src/Controller/AdminTrainerController.php
+++ src/Controller/AdminTrainerController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Trainer;
+use App\Form\TrainerType;
 use App\Repository\TrainerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -17,6 +20,25 @@ class AdminTrainerController extends AbstractController
 
         return $this->render('admin/trainer/list.html.twig', [
             'trainers' => $trainers
+        ]);
+    }
+
+    #[Route('/admin/trainer/create', name: 'app_trainer_create')]
+    public function createTrainer(Request $request):Response {
+
+        $trainer = new Trainer();
+        $form = $this->createForm(TrainerType::class, $trainer);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()  ) {
+          
+            dd($form->getData());
+
+            return $this->redirectToRoute('app_admin_trainer');
+        }
+
+        return $this->render('admin/trainer/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
