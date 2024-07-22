a68d90a164800db70c25074e9ac4bd8ec9925571
Antoine07 <antoine.lucsko@gmail.com>
2024-07-22 15:59:03 +0200

    delete form article

--- src/Service/ModelDVFService.php
+++ src/Service/ModelDVFService.php
@@ -3,7 +3,6 @@
 namespace App\Service;
 
 use Rubix\ML\Datasets\Unlabeled;
-use Rubix\ML\Loggers\Logger;
 use Rubix\ML\PersistentModel;
 use Rubix\ML\Persisters\Filesystem;
 
@@ -15,7 +14,7 @@ class ModelDVFService
     const DIR_MODELS  = __DIR__ . '/saved_models';
     private ?PersistentModel $estimator = null;
 
-    private string $filename = 'dvf_model_84.rbx' ;
+    private string $filename = 'dvf_model_84.rbx';
 
     function loadSavedModel()
     {
@@ -30,4 +29,3 @@ class ModelDVFService
         return round($predictions[0], 2);
     }
 }
-
